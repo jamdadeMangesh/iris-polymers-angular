edd53e166610860679f322b939da9fcd23d7c49e
“jamdadeMangesh” <jamdademangesh1993@gmail.com>
2018-05-31 15:14:32 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'
+import { routes } from './app.routing';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch : 'full'
+  },
+  {
+     path: 'home',
+     component : HomeComponent
+  },
+  {
+     path: 'about',
+     component : AboutComponent
+  }
+];
+
+export class AppRoutingModule { }
