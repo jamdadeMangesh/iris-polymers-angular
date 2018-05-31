6d9d6aa2a03094986604ec6e4e3362ee86f4c1be
“jamdadeMangesh” <jamdademangesh1993@gmail.com>
2018-05-31 16:00:25 +0530

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { routes } from './app.routing';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
